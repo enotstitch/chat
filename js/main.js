e47dfcffa94dd18ac2b71e1a5321531b442582c8
ZayRexan <mogilevkostya@yandex.ru>
2023-02-05 23:00:13 +0700

    Куки-жуки

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
+import Cookies from 'js-cookie';
+
 const settingsBtn = document.querySelector('.chat__settings');
+const enterCodeBtn = document.querySelector('.chat__send--enter');
 const closeBtns = document.querySelectorAll('.modal__close');
 let modal;
 let modalWrap;
 
 function openModal(id) {
-  modal = document.getElementById(id);
-  modalWrap = modal.querySelector('.modal__wrap');
+	modal = document.getElementById(id);
+	modalWrap = modal.querySelector('.modal__wrap');
 
-  modal.classList.add('open');
-  modalWrap.classList.add('open');
+	modal.classList.add('open');
+	modalWrap.classList.add('open');
 }
 
 function closeModal() {
-  modal.classList.remove('open');
-  modalWrap.classList.remove('open');
+	modal.classList.remove('open');
+	modalWrap.classList.remove('open');
 }
 
 settingsBtn.addEventListener('click', () => {
-  openModal('settings');
+	openModal('settings');
+});
+
+enterCodeBtn.addEventListener('click', () => {
+	openModal('confirmation');
 });
 
 closeBtns.forEach((btn) => {
-  btn.addEventListener('click', closeModal);
+	btn.addEventListener('click', closeModal);
 });
 
 // !
@@ -29,31 +36,31 @@ closeBtns.forEach((btn) => {
 const chatForm = document.querySelector('.chat__form');
 
 function sendMessage() {
-  const dialogue = document.querySelector('.dialogue');
-  const message = document.getElementById('myMessage');
-  let messageBlock = document.createElement('div');
-  const messageInput = document.querySelector('.chat__message');
+	const dialogue = document.querySelector('.dialogue');
+	const message = document.getElementById('myMessage');
+	let messageBlock = document.createElement('div');
+	const messageInput = document.querySelector('.chat__message');
 
-  if (messageInput.value === '') {
-    return;
-  }
+	if (messageInput.value === '') {
+		return;
+	}
 
-  let messageText = messageInput.value;
+	let messageText = messageInput.value;
 
-  messageBlock.className =
-    'dialogue__message dialogue__message--my message message--sent';
+	messageBlock.className =
+		'dialogue__message dialogue__message--my message message--sent';
 
-  messageBlock.append(message.content.cloneNode(true));
+	messageBlock.append(message.content.cloneNode(true));
 
-  dialogue.append(messageBlock);
-  let messageBlockText = messageBlock.querySelector('.message__text');
-  messageBlockText.textContent = messageText;
-  messageInput.value = '';
+	dialogue.append(messageBlock);
+	let messageBlockText = messageBlock.querySelector('.message__text');
+	messageBlockText.textContent = messageText;
+	messageInput.value = '';
 }
 
 chatForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  sendMessage();
+	event.preventDefault();
+	sendMessage();
 });
 
 // !
@@ -61,9 +68,64 @@ chatForm.addEventListener('submit', (event) => {
 const menuOpenBtn = document.querySelector('.chat__menu-btn');
 
 function openMenu() {
-  const menu = document.querySelector('.chat__menu');
-  menu.classList.toggle('menu-open');
-  event.target.classList.toggle('chat__menu-btn--active');
+	const menu = document.querySelector('.chat__menu');
+	menu.classList.toggle('menu-open');
+	event.target.classList.toggle('chat__menu-btn--active');
 }
 
 menuOpenBtn.addEventListener('click', openMenu);
+
+// !
+
+const url = 'https://edu.strada.one/api/user';
+let user;
+
+async function getCode() {
+	const emailInput = document.querySelector('.modal__email');
+	user.email = emailInput.value;
+
+	let response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+		body: JSON.stringify(user),
+	});
+
+	let result = await response.json();
+	console.log(result);
+}
+
+let btnCode = document.querySelector('.chat__send--code');
+
+btnCode.addEventListener('click', (event) => {
+	event.preventDefault();
+	getCode();
+});
+
+// !
+
+async function login() {
+	const tokenInput = document.querySelector('.chat__send--enter-code');
+	const token = tokenInput.value;
+	Cookies.set('token', token);
+
+	let response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Authorization': `Bearer ${token}`,
+		},
+		body: JSON.stringify(user),
+	});
+
+	let result = await response.json();
+	console.log(result);
+}
+
+const authorizationBtn = document.querySelector('.chat__send--authorization');
+
+authorizationBtn.addEventListener('click', (event) => {
+	event.preventDefault();
+	login();
+});
